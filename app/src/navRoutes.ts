e8be88aaaa9d757fc7ccbd4307ae6b9d63338388
matiNap <44534427+matiNap@users.noreply.github.com>
2020-08-20 19:59:58 +0200

    Update navRoutes.ts

--- app/src/navRoutes.ts
+++ app/src/navRoutes.ts
@@ -2,4 +2,4 @@ export const MAIN = "/portfolio";
 export const SKILLS = `${MAIN}/skills`;
 export const EXP = `${MAIN}/experience`;
 export const PROJECTS = `${MAIN}/projects`;
-export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/resume.pdf?alt=media&token=eeabd82c-ebef-401d-8df3-cb71e8a445cd`;
+export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/resume.pdf?alt=media&token=3c124393-08e0-4755-9e46-e41df7d98387`;
