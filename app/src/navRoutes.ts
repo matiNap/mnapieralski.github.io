7aefd7066109040e7033f43f66b6ad9a6953782d
matiNap <44534427+matiNap@users.noreply.github.com>
2020-08-25 22:50:22 +0200

    Fix nav routes

--- app/src/navRoutes.ts
+++ app/src/navRoutes.ts
@@ -2,4 +2,4 @@ export const MAIN = "/portfolio";
 export const SKILLS = `${MAIN}/skills`;
 export const EXP = `${MAIN}/experience`;
 export const PROJECTS = `${MAIN}/projects`;
-export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/resume.pdf?alt=media&token=3c124393-08e0-4755-9e46-e41df7d98387`;
+export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/resumse.pdf?alt=media&token=bb753967-4f61-44cb-adda-26737e2cd668`;
