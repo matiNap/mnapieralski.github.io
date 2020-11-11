3b170a8aa3092f225bb1f96b89db4683c3c6f7bf
matiNap <44534427+matiNap@users.noreply.github.com>
2020-11-11 13:10:13 +0100

    Update resume

--- app/src/navRoutes.ts
+++ app/src/navRoutes.ts
@@ -2,4 +2,4 @@ export const MAIN = "/portfolio";
 export const SKILLS = `${MAIN}/skills`;
 export const EXP = `${MAIN}/experience`;
 export const PROJECTS = `${MAIN}/projects`;
-export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/ResumeMateuszNapieralski.pdf?alt=media&token=1b85bf90-3312-46f1-8734-6e6e19f3c4ae`;
+export const RESUME = `https://firebasestorage.googleapis.com/v0/b/portfolio-d8494.appspot.com/o/Resume%20Mateusz%20Napieralski.pdf?alt=media&token=a1cfc55c-a0e4-49d8-83e6-70e924857094`;
